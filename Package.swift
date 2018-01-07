259a0575c7db801636caaa3fca3448e37f826290
TheCB4 <cavelle@thecb4.io>
2018-01-06 20:59:22 -0500

    Fixed MotherShip dependency

--- Package.swift
+++ Package.swift
@@ -5,11 +5,16 @@ import PackageDescription
 
 let package = Package(
   name: "MotherShip-CLI",
+  products: [
+    .executable(
+      name: "mothership",
+      targets: ["MotherShip-CLI"]
+    )
+  ],
   dependencies: [
       // Dependencies declare other packages that this package depends on.
       // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "git@github.com:thecb4/MotherShip.git", .branch("master")),
-    .package(url: "git@github.com:thecb4/HyperSpace.git", .branch("master")),
     .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from:"0.8.0")),
     .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .branch("master")),
     .package(url: "https://github.com/JohnSundell/Files.git",.branch("master"))
@@ -20,6 +25,6 @@ let package = Package(
       // Targets can depend on other targets in this package, and on products in packages which this package depends on.
       .target(
           name: "MotherShip-CLI",
-          dependencies: ["MotherShip","HyperSpace", "Commander","CryptoSwift", "Files" ]),
+          dependencies: ["MotherShip", "CryptoSwift", "Files", "Commander"]),
   ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Mothership-CLI
 
 Command line interface for [MotherShip](https://github.com/thecb4/MotherShip) library
+
+## Install Instructions
+```zsh
+$ make update build install
+```
